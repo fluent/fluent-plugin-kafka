be01777ab7d8707ba95f7ac8c76e83e5ee6dab5b
Patrick Susbilla <sesusbilla@voyagerinnovation.com>
2014-05-09 00:27:19 +0800

    Added option to add tag and time

--- lib/fluent/plugin/out_kafka_buffered.rb
+++ lib/fluent/plugin/out_kafka_buffered.rb
@@ -4,6 +4,7 @@ class Fluent::KafkaOutputBuffered < Fluent::BufferedOutput
   def initialize
     super
     require 'poseidon'
+    @localtime = false
   end
 
   config_param :brokers, :string, :default => 'localhost:9092'
@@ -11,6 +12,9 @@ class Fluent::KafkaOutputBuffered < Fluent::BufferedOutput
   config_param :default_partition, :integer, :default => 0
   config_param :client_id, :string, :default => 'kafka'
   config_param :output_data_type, :string, :default => 'json'
+  config_param :tag_key, :string, :default => nil
+  config_param :time_key, :string, :default => nil
+  config_param :time_format, :string, :default => nil
   attr_accessor :output_data_type
   attr_accessor :field_separator
 
@@ -41,6 +45,15 @@ class Fluent::KafkaOutputBuffered < Fluent::BufferedOutput
                          else
                            nil
                          end
+
+  if @time_key
+      if @time_format
+        tf = Fluent::TimeFormatter.new(@time_format, @localtime)
+        @time_format_proc = tf.method(:format)
+      else
+        @time_format_proc = Proc.new { |time| time.to_s }
+      end
+    end
   end
 
   def start
@@ -52,6 +65,12 @@ class Fluent::KafkaOutputBuffered < Fluent::BufferedOutput
   end
 
   def format(tag, time, record)
+    if @time_key
+      record[@time_key] = @time_format_proc.call(time)
+    end
+    if @tag_key
+      record[@tag_key] = tag
+    end
     [tag, time, record].to_msgpack
   end
 
