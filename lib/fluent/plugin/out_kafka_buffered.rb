6490cec7f406a8186686d8adc13ceddb971b9c75
Mahito <earthdragon77@gmail.com>
2016-02-11 05:58:00 +0900

    Close producer connection when refresh producer
    I hit a problem same as #38 .
    It seems that new producer is created without closing old producer connection.
    This PR is closing producer connection when refresh producer.

--- lib/fluent/plugin/out_kafka_buffered.rb
+++ lib/fluent/plugin/out_kafka_buffered.rb
@@ -60,6 +60,7 @@ DESC
         broker = Yajl.load(z.get(:path => "/brokers/ids/#{id}")[:data])
         @seed_brokers.push("#{broker['host']}:#{broker['port']}")
       end
+      z.close
       log.info "brokers has been refreshed via Zookeeper: #{@seed_brokers}"
     end
     begin
@@ -176,6 +177,7 @@ DESC
     end
   rescue Exception => e
     log.warn "Send exception occurred: #{e}"
+    @producer.close if @producer
     refresh_producer()
     # Raise exception to retry sendind messages
     raise e
