1a47c012e5d18d68e0b48f8659bea529f3d2d7c3
Steven Fitzpatrick <steven.fitzpatrick@att.com>
2019-12-05 11:13:51 -0600

    Add sasl_over_ssl parameter to sasl_plain consumer plugins
    This change adds the sasl_over_ssl parameter to the consumer
    plugins, in order to allow sasl_over_ssl to be disabled when
    using the plain sasl authentication.

--- lib/fluent/plugin/in_kafka_group.rb
+++ lib/fluent/plugin/in_kafka_group.rb
@@ -170,7 +170,8 @@ class Fluent::KafkaGroupInput < Fluent::Input
     elsif @username != nil && @password != nil
       @kafka = Kafka.new(seed_brokers: @brokers, client_id: @client_id, logger: logger, connect_timeout: @connect_timeout, socket_timeout: @socket_timeout, ssl_ca_cert: read_ssl_file(@ssl_ca_cert),
                          ssl_client_cert: read_ssl_file(@ssl_client_cert), ssl_client_cert_key: read_ssl_file(@ssl_client_cert_key),
-                         ssl_ca_certs_from_system: @ssl_ca_certs_from_system, sasl_plain_username: @username, sasl_plain_password: @password)
+                         ssl_ca_certs_from_system: @ssl_ca_certs_from_system, sasl_plain_username: @username, sasl_plain_password: @password,
+                         sasl_over_ssl: @sasl_over_ssl)
     else
       @kafka = Kafka.new(seed_brokers: @brokers, client_id: @client_id, logger: logger, connect_timeout: @connect_timeout, socket_timeout: @socket_timeout, ssl_ca_cert: read_ssl_file(@ssl_ca_cert),
                          ssl_client_cert: read_ssl_file(@ssl_client_cert), ssl_client_cert_key: read_ssl_file(@ssl_client_cert_key),
