707cff013789c03452e40e7042e62f517b6cb062
gigkokman <kann.kupp@gmail.com>
2020-06-04 16:36:36 +0700

    Add custom time key when time_source is record
    Signed-off-by: gigkokman <kann.kupp@gmail.com>

--- lib/fluent/plugin/in_kafka.rb
+++ lib/fluent/plugin/in_kafka.rb
@@ -39,6 +39,8 @@ class Fluent::KafkaInput < Fluent::Input
                :deprecated => "Use 'time_source record' instead."
   config_param :time_source, :enum, :list => [:now, :kafka, :record], :default => :now,
                :desc => "Source for message timestamp."
+  config_param :record_time_key, :string, :default => 'time',
+               :desc => "Time field when time_source is 'record'"
   config_param :get_kafka_client_log, :bool, :default => false
   config_param :time_format, :string, :default => nil,
                :desc => "Time format to be used to parse 'time' field."
@@ -114,8 +116,10 @@ class Fluent::KafkaInput < Fluent::Input
     @parser_proc = setup_parser
 
     @time_source = :record if @use_record_time
+    @time_key = 'time'
 
     if @time_source == :record and @time_format
+      @time_key = @record_time_key if @record_time_key
       if defined?(Fluent::TimeParser)
         @time_parser = Fluent::TimeParser.new(@time_format)
       else
@@ -292,9 +296,9 @@ class Fluent::KafkaInput < Fluent::Input
             record_time = Fluent::Engine.now
           when :record
             if @time_format
-              record_time = @time_parser.parse(record['time'])
+              record_time = @time_parser.parse(record[@time_key])
             else
-              record_time = record['time']
+              record_time = record[@time_key]
             end
           else
             $log.fatal "BUG: invalid time_source: #{@time_source}"
